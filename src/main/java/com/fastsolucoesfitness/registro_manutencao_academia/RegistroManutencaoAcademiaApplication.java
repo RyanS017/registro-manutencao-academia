f78bff56c01a2fb16af1715d19eae2ff57ab130b
Ryan Silva <ryansilva017@gmail.com>
2024-08-25 11:43:58 -0300

    criacao projeto

---
+++ src/main/java/com/fastsolucoesfitness/registro_manutencao_academia/RegistroManutencaoAcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.fastsolucoesfitness.registro_manutencao_academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroManutencaoAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroManutencaoAcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastsolucoesfitness/registro_manutencao_academia/RegistroManutencaoAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastsolucoesfitness.registro_manutencao_academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroManutencaoAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
